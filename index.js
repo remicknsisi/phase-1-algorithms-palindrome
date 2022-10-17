226432c3007bc31a92706d5d2b118a17695a4f75
Sienna <remicknsisi@gmail.com>
2022-10-17 15:48:57 -0400

    commit tree 9911052372e6132be6d6f19241fc99c07e55f4e3

--- index.js
+++ index.js
@@ -1,25 +1,44 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+  let backwardsWord = word.split('').reverse().join('')
+
+  if (word === backwardsWord){
+    return true
+  } else {
+    return false
+  }
 }
 
 /* 
-  Add your pseudocode here
+
+  create function named 'isPalindrome' whose argument = string
+
+  break that string out into its individual letters and assort them in an array
+    reverse that array and join the letters
+
+  use if / else statement to determine the return value of the function 
+    if the new value is equal to the value passed in, return true. if not, return false.
+
 */
 
 /*
-  Add written explanation of your solution here
+
+  The function isPalindrome takes in a string. I create a variable to define the backwards version of the input. I do this by splitting the string to parse it into an array of letters, then use the array reverse() method to change the order. Finally, I join the array elements to create a new string. I then use an if / else statement to check if the backwards version matches the value passed in. If it does, the function returns true. If it does not, the function returns false.
+
 */
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
-  // add your own custom tests in here
   console.log("Expecting: true");
   console.log("=>", isPalindrome("racecar"));
 
-  console.log("");
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("abba"))
 
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("a"))
 }
 
 module.exports = isPalindrome;
